b5935fc51b6f141920c4013f403149c1e8701e20
Michael Clayton Noble <makos.code@gmail.com>
2020-12-04 01:13:56 -0800

    Implement map with an array

---
+++ W3D5/map.rb
@@ -0,0 +1,40 @@
+class Map
+
+
+  def initialize
+    @array_map = []
+  end
+
+  def set(key, value)
+    key_flag = false
+
+    @array_map.each do |pair|
+      if pair.include?(key)
+        pair[1] = value
+        key_flag = true
+        break
+      end
+    end
+    @array_map.push([key,value]) unless true
+  end
+
+  def get(key)
+    @array_map.each{|pair| return pair[1] if pair.include?(key)}
+  end
+
+  def delete(key)
+    @array_map.each{|pair| array_map.delete(pair) if pair.include?(key)}
+  end
+
+  def show
+    deep_dup(@array_map)
+  end
+
+  def inspect
+    @array_map.to_s
+  end
+
+   def deep_dup(arr)
+    arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+  end  
+end
