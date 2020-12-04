f437fd7363c6b23be95dff5195ea45d320abc083
Michael Clayton Noble <makos.code@gmail.com>
2020-12-04 00:53:09 -0800

    Implemented Queue Class

---
+++ W3D5/queue.rb
@@ -0,0 +1,22 @@
+class Queue
+
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue.push(el)
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def peek
+    @queue.first
+  end
+
+  def inspect
+    @queue.to_s
+  end
+end
