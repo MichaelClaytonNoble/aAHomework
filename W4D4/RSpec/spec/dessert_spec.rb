8058610c6be05966b9014e80f441d914efdf011b
Michael Clayton Noble <makos.code@gmail.com>
2020-12-10 02:04:56 -0800

    create unit tests to test ::Dessert

--- W4D4/RSpec/spec/dessert_spec.rb
+++ W4D4/RSpec/spec/dessert_spec.rb
@@ -6,37 +6,65 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
+  subject(:dessert) {Dessert.new("Green Tea Icecream", 100, chef) }
   let(:chef) { double("chef") }
 
   describe "#initialize" do
-    it "sets a type"
+  
+    it "sets a type" do 
+      expect(dessert.type).to eq("Green Tea Icecream")
+    end
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(100)
+    end 
+    it "starts ingredients as an empty array" do 
+      expect(dessert.ingredients).to be_empty
+    end
 
-    it "sets a quantity"
-
-    it "starts ingredients as an empty array"
-
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do 
+      expect { Dessert.new("Ice", "100", chef) }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do 
+      expect(dessert.ingredients).to be_empty
+      dessert.add_ingredient("Heavy Cream")
+      expect(dessert.ingredients).to include("Heavy Cream")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      ingredients_array = dessert.ingredients
+      dessert.mix!
+      expect(dessert.ingredients).to eq(ingredients_array)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
 
-    it "raises an error if the amount is greater than the quantity"
+    it "subtracts an amount from the quantity" do 
+      dessert.eat(99)
+      expect(dessert.quantity).to eq(1)
+    end
+
+    it "raises an error if the amount is greater than the quantity" do 
+      expect {dessert.eat(100000)}.to raise_error('not enough left!')
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Mako Nobo")
+      expect(dessert.serve).to include("Mako Nobo")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(dessert)
+      dessert.make_more
+    end
   end
 end
