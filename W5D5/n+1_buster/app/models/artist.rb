e5f7074b0d67c2d1062de147e4fab3275ee14377
Michael Clayton Noble <makos.code@gmail.com>
2020-12-27 01:30:27 -0800

    update #better_tracks_query, also add solutions file for n+1 buster

--- W5D5/n+1_buster/app/models/artist.rb
+++ W5D5/n+1_buster/app/models/artist.rb
@@ -16,13 +16,28 @@ class Artist < ApplicationRecord
 
   def better_tracks_query
     # TODO: your code here
-    albums = self.albums.includes(:tracks)
+    #this include seems to optimize the query however
+    #a join would be more appropriate since we
+    #do not require any information about each track
+    #we only want to count 
+    #albums = self.albums.includes(:tracks)
     
-    tracks_count = {}
+    
+    albums = self
+    .albums
+    .select("albums.id, albums.title, COUNT(*) as tracks_count")
+    .joins(:tracks)
+    .group("albums.id")
+
+    #notice that the aggregate COUNT function named tracks_count
+    #has become a method name that we can use to count the number
+    #of tracks on any particular album
+
+    album_catalog = {}
     albums.each do |album|
-      tracks_count[album.title] = album.tracks.length
+      album_catalog[album.title] = album.tracks_count
     end
 
-    tracks_count
+    album_catalog
   end
 end
