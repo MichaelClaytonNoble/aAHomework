278b9de79dd4f6159d60822dcd3f47e60c1ae191
Michael Clayton Noble <makos.code@gmail.com>
2021-01-04 08:11:38 -0800

    start homework

--- W7D2/app_name/app/models/user.rb
+++ W7D2/app_name/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :username, :session_token, presence: true
+  validates :password_digest, presence: true, message: 'Password can''t be blank' 
+  validate :password_digest_length
+
+  private
+  def password_digest_length
+  end 
+
+  
 end
