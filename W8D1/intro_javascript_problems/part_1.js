01147e63d90367ee1c4a8aca3b7efea98bf15196
Michael Clayton Noble <makos.code@gmail.com>
2021-01-18 14:09:34 -0800

    W8D1 homework

---
+++ W8D1/intro_javascript_problems/part_1.js
@@ -0,0 +1,20 @@
+
+//madLib
+function madLib(verb, adjective, noun){
+  console.log(`We shall ${verb.toUpperCase()} the ${adjective.toUpperCase} ${noun.toUpperCase}.`);
+}
+
+// madLib("make", "best", "guac");
+
+//isSubstring
+
+isSubstring = function (searchString, subString){
+  return (searchString.includes(subString));
+}
+
+// console.log(isSubstring("time to program", "time"));
+// > true
+// console.log(isSubstring("Jump for joy", "joys"));
+// > false
+
+
