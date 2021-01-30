26f73369374928215aa6cad4c70cacd557e4248c
Michael Clayton Noble <makos.code@gmail.com>
2021-01-30 14:10:42 -0800

    create simple calculator using react

--- W10D2/react_calculator/calculator.jsx
+++ W10D2/react_calculator/calculator.jsx
@@ -8,6 +8,11 @@ class Calculator extends React.Component {
     this.setNum1 = this.setNum1.bind(this);
     this.setNum2 = this.setNum2.bind(this);
     this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
+    this.multiply = this.multiply.bind(this);
+    this.divide = this.divide.bind(this);
+    this.clear = this.clear.bind(this);
+
   }
 
   setNum1(e){
@@ -19,22 +24,51 @@ class Calculator extends React.Component {
     this.setState({num2: num2});
   }
 
+  clear(e){
+    e.preventDefault();
+    this.setState({ result: 0, num1: "", num2: "" });
+  }
+
   add(e){
     e.preventDefault();
     const {num1, num2} = this.state; 
     const result = num1+num2;
     this.setState({result})
   }
+  subtract(e){
+    e.preventDefault();
+    const {num1, num2} = this.state; 
+    const result = num1-num2;
+    this.setState({result})
+  }
+  multiply(e){
+    e.preventDefault();
+    const {num1, num2} = this.state; 
+    const result = num1*num2;
+    this.setState({result})
+  }
+  divide(e){
+    e.preventDefault();
+    const {num1, num2} = this.state; 
+    const result = num1/num2;
+    this.setState({result})
+  }
+
   render (){
+    const {num1, num2, result} = this.state; 
     return(
 
       <div>
-        <h1>{this.state.result}</h1>
-
-        <input name="num1" value={this.state.num1} onChange={this.setNum1}/>
-        <input name="num2" value={this.state.num2} onChange={this.setNum2}/>
+        <h1>{result}</h1>
+        <input name="num1" value={num1} onChange={this.setNum1}/>
+        <input name="num2" value={num2} onChange={this.setNum2}/>
 
         <button onClick={this.add}>+</button>
+        <button onClick={this.subtract}>-</button>
+        <button onClick={this.multiply}>*</button>
+        <button onClick={this.divide}>/</button>
+        <button onClick={this.clear}>clear</button>
+
       </div>
 
     );
