383cd5e5db28d009f082adae597b83a57a031067
Michael Clayton Noble <makos.code@gmail.com>
2021-02-05 16:37:58 -0800

    create two ajax calls to open weather

--- W9D4/first_ajax_request/js/index.js
+++ W9D4/first_ajax_request/js/index.js
@@ -1,5 +1,24 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
-
+$.ajax({
+  method: "GET",
+  url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b"
+}).then((data) => console.log(data));
 // Add another console log here, outside your AJAX request
+
+const ajax2 = ()=> { 
+
+  return( 
+
+    $.ajax({
+      method: "GET",
+      url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b",
+      success(data) {
+        console.log(data);
+      }
+    })
+  );
+}
+
+window.ajax2 = ajax2;
